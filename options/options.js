980ee079995f77595a0514071c1499b43c4d4b3e
Crowdbotics <team@crowdbotics.com>
2021-10-18 16:06:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kwajgram_31512",
+  url: "https://kwajgram_31512.botics.co",
+  api: "https://kwajgram_31512.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
